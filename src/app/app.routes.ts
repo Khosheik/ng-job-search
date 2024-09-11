759333032475a332cb498163b29a95f97f9e95bf
maite <maite.delgado42@gmail.com>
2024-09-11 11:39:32 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { JobsComponent } from './jobs/jobs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: JobsComponent }, 
+    { path: 'jobs', component: JobsComponent},
+    { path: 'favorites', component: JobsComponent},
+];
