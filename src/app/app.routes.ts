e03221bd0cd2f2860636f3ddeb75aefce78a6acb
maite <maite.delgado42@gmail.com>
2024-09-12 08:00:27 +0200

    styling menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { JobsComponent } from './jobs/jobs.component';
 
 export const routes: Routes = [
-    { path: '', component: JobsComponent }, 
     { path: 'jobs', component: JobsComponent},
     { path: 'favorites', component: JobsComponent},
+    { path: '', redirectTo: 'jobs', pathMatch: 'full' },
 ];
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'njs-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  
 }
