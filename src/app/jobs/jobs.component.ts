dc4d8c73296d249bce30f9b804c2f49309217426
maite <maite.delgado42@gmail.com>
2024-09-12 15:46:39 +0200

    no fav message

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -3,7 +3,7 @@ import { JobsService } from '../jobs.service';
 import { AsyncPipe } from '@angular/common';
 import { JobComponent } from '../job/job.component';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { map, of } from 'rxjs';
 
 @Component({
   selector: 'njs-jobs',
@@ -15,6 +15,7 @@ import { map } from 'rxjs';
 export class JobsComponent {
   path$ = this.route.url.pipe(map(result => result[0].path));
   jobs$ = this.jobsService.getJobs(this.path$);
+  favorites = this.jobsService.favoriteJobs;
 
   constructor(private jobsService: JobsService, private route: ActivatedRoute){
   }
